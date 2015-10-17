9b52feafc895b59e13a2f748146237bff1060d8a
Ubuntu <ubuntu@ip-172-31-34-97.us-west-2.compute.internal>
2015-10-17 21:24:04 +0000

    logspam

--- static/js/react/app.js
+++ static/js/react/app.js
@@ -4,7 +4,6 @@ var React = require('react'),
 
 var Review = React.createClass({
   render: function() {
-    console.log('render revinner', this.props.review);
     return (
       <div className="panel panel-primary">
         <div className="page-header">
@@ -31,7 +30,6 @@ var Reviews = React.createClass({
     }.bind(this))
   },
   render: function() {
-    console.log('render reviews');
     return (
       <div>
         {this.state.reviews.map(function(review) {
@@ -50,7 +48,6 @@ var App = React.createClass({
   },
 
   render: function() {
-    console.log('render app');
     var fixtures = [
       {label: 'Old Elbe Tunnel, Hamburg', location: {lat: 53.5459, lng: 9.966576}},
       {label: 'Reeperbahn, Hamburg', location: {lat: 53.5495629, lng: 9.9625838}},
